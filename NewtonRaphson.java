d0a0e4bb2f0438dd56c847bd0e23bf975400246b
juanfig99 <juanfigueroa201799@gmail.com>
2024-06-28 20:06:48 -0500

    Add files via upload

---
+++ NewtonRaphson.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+
+public class NewtonRaphson
+{
+	static final double EPSILON = 0.0001;
+	double x0 = 0.1;
+	double term;
+	
+	public NewtonRaphson(double t, boolean ispv, double pv, double fv)
+	{
+		
+	}
+	
+	// method to iterate the Newton Raphson method
+	// checks to see if the function is the present value function
+	// returns the root of the given function
+	public double iterateNR()
+	{
+		
+	}
+	
+	public double f(double x)
+	{
+		
+	}
+	
+	public double fPrime(double x)
+	{
+		
+	}
+}
