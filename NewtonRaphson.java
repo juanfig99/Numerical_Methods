58f6edb6d8b6055b75618d2df16803bdac936de2
juanfig99 <juanfigueroa201799@gmail.com>
2024-07-01 23:02:40 -0500

    Update NewtonRaphson.java
    Works with Algebraic.java

--- NewtonRaphson.java
+++ NewtonRaphson.java
@@ -1,32 +1,57 @@
-// Updating so that it is compatible with Algebraic.java
 import java.lang.Math;
 
 public class NewtonRaphson
 {
-	static final double EPSILON = 0.0001;
-	double x0 = 0.1;
-	double term;
+	static final double EPSILON = 0.00000000001;
+	double x0;
 	
-	public NewtonRaphson(double t, boolean ispv, double pv, double fv)
+	public NewtonRaphson()
 	{
-		
+		x0 = 0.1;
 	}
 	
 	// method to iterate the Newton Raphson method
-	// checks to see if the function is the present value function
 	// returns the root of the given function
-	public double iterateNR()
+	public double iterateNR(Algebraic[] expr)
 	{
+		double g, gPrime;
+		double x1 = 1;
 		
+		for (int i = 0; i < Integer.MAX_VALUE; i++)
+		{
+			g = f(expr,x0);
+			gPrime = fPrime(expr,x0);
+			
+			x1 = x0 - g/gPrime;
+			
+			if(Math.abs(x1-x0) <= EPSILON)
+			{
+				System.out.println("Iteration stops at i = "+i);
+				return x1;
+			}
+			x0 = x1;
+		}
+		System.out.println("Could not find root within classes break tolerance");
+		return 0;
 	}
 	
-	public double f(double x)
+	public double f(Algebraic[] arr, double x)
 	{
-		
+		double eval = 0;
+		for(int i = 0; i < arr.length; i++)
+		{
+			eval += arr[i].evalTermAt(x);
+		}
+		return eval;
 	}
 	
-	public double fPrime(double x)
+	public double fPrime(Algebraic[] arr, double x)
 	{
-		
+		double eval = 0;
+		for(int i = 0; i < arr.length; i++)
+		{
+			eval += arr[i].evalTermsDerivativeAt(x);
+		}
+		return eval;
 	}
 }
